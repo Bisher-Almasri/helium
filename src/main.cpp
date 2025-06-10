1d5207c8cb8129e1de46a6e9b594634f8bd87a96
BISHER-AL-MASRI <bbgyttv@gmail.com>
2025-06-10 10:39:36 -0400

    e

---
+++ src/main.cpp
@@ -0,0 +1,170 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <filesystem>
+
+enum class TokenType
+{
+    RETURN,
+    INT_LIT,
+    SEMI
+};
+
+struct Token
+{
+    TokenType type;
+    std::optional<std::string> value;
+};
+
+// RETURNS VECTOR OF TOKENS
+std::vector<Token> tokenize(std::string &text)
+{
+    std::vector<Token> tokens = {};
+
+    std::string buf;
+    for (int i = 0; i < text.length(); )
+    {
+        char c = text.at(i);
+        if (std::isalpha(c))
+        {
+            buf.push_back(c);
+            i++;
+
+            while (i < text.length() && std::isalnum(text.at(i)))
+            {
+                buf.push_back(text.at(i));
+                i++;
+            }
+
+            if (buf == "return")
+            {
+                tokens.push_back({.type = TokenType::RETURN});
+                buf.clear();
+            } else
+            {
+                std::cerr << "Error: Unknown keyword '" << buf << "'" << std::endl;
+                exit(EXIT_FAILURE);
+            }
+        }
+        else if (std::isdigit(c))
+        {
+            buf.push_back(c);
+            i++;
+
+            while (i < text.length() && std::isdigit(text.at(i)))
+            {
+                buf.push_back(text.at(i));
+                i++;
+            }
+            tokens.push_back({.type = TokenType::INT_LIT, .value = buf});
+            buf.clear();
+        }
+        else if (c == ';')
+        {
+            tokens.push_back({.type = TokenType::SEMI});
+            i++;
+        }
+        else if (std::isspace(c))
+        {
+            i++;
+            continue;
+        } else
+        {
+            std::cerr << "Error: Unexpected character '" << c << "'" << std::endl;
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    return tokens;
+}
+
+// TODO: ADD PARSER
+
+std::string tokens_to_asm(std::vector<Token> &tokens)
+{
+    std::stringstream output;
+    // output << "global _start\n_start:\n";
+    output << "global _main\n_main:\n";
+    for (int i = 0; i < tokens.size(); ++i)
+    {
+        const Token &token = tokens.at(i);
+        if (token.type == TokenType::RETURN)
+        {
+            if (i+1 < tokens.size() && tokens[i+1].type == TokenType::INT_LIT)
+            {
+                if (i+2 < tokens.size() && tokens[i+2].type == TokenType::SEMI)
+                {
+                    // output << "    mov rax, 60\n";
+                    output << "    mov rax, 0x2000001\n";
+                    output << "    mov rdi, " << tokens[i + 1].value.value() << "\n";
+                    output << "    syscall";
+                }
+            }
+        }
+    }
+
+    return output.str();
+}
+
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        std::cerr << "Usage: " << argv[0] << " <filename>" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    std::filesystem::path input_path(argv[1]);
+    std::string base_filename = input_path.stem();
+
+    std::string contents;
+    {
+        std::stringstream contents_stream;
+        std::fstream input(argv[1], std::ios::in);
+        if (!input.is_open()) {
+            std::cerr << "Error: Could not open file " << argv[1] << std::endl;
+            return EXIT_FAILURE;
+        }
+        contents_stream << input.rdbuf();
+        contents = contents_stream.str();
+    }
+
+    std::vector<Token> tokens = tokenize(contents);
+
+    std::string asm_filename = base_filename + ".asm";
+    {
+        std::fstream file(asm_filename, std::ios::out);
+        if (!file.is_open()) {
+            std::cerr << "Error: Could not create " << asm_filename << std::endl;
+            return EXIT_FAILURE;
+        }
+        file << tokens_to_asm(tokens);
+    }
+
+    std::string obj_filename = base_filename + ".o";
+    const std::string& exe_filename = base_filename;
+
+    std::string nasmcmd = "nasm -f macho64 " + asm_filename + " -o " + obj_filename;
+    std::string ldcmd = "ld -platform_version macos 10.7.0 10.15.0 -syslibroot $(xcrun --show-sdk-path) " +
+                        obj_filename + " -o " + exe_filename + " -lSystem";
+    std::string rmcmd = "rm -f " + obj_filename + " " + asm_filename;
+
+    std::cout << "Executing: " << nasmcmd << std::endl;
+    int nasm_result = system(nasmcmd.c_str());
+    if (nasm_result != 0) {
+        std::cerr << "nasm failed with error " << nasm_result << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    std::cout << "Executing: " << ldcmd << std::endl;
+    int ld_result = system(ldcmd.c_str());
+    if (ld_result != 0) {
+        std::cerr << "ld failed with error " << ld_result << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    system(rmcmd.c_str());
+    return EXIT_SUCCESS;
+}
