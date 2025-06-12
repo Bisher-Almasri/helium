af342a91d33276ee808c04d0f8a3dfa0eb50dcd9
Bisher Almasri <bbgyttv@gmail.com>
2025-06-11 21:15:48 -0400

    kill me kill me kill me kill me kill me kill me killl me kill me kill me kill me kill me kill me kill me kill me kill me kill me kill me killl me kill me kill me kill me kill mekill me kill me kill me kill me kill me kill me killl me kill me kill me kill me kill me

--- src/main.cpp
+++ src/main.cpp
@@ -2,48 +2,41 @@
 // Created by Bisher Almasri on 2025-06-10.
 //
 
-#include <iostream>
+#include "generation.hpp"
+#include "parser.hpp"
+#include "tokenization.hpp"
+
+#include <filesystem>
 #include <fstream>
+#include <iostream>
 #include <sstream>
 #include <vector>
-#include <filesystem>
-
-#include "tokenizer.hpp"
-
-// RETURNS VECTOR OF TOKENS
-std::vector<Token> tokenize(std::string &text)
-{
-
-}
 
 // TODO: ADD PARSER
 
-std::string tokens_to_asm(std::vector<Token> &tokens)
-{
-    std::stringstream output;
-    // output << "global _start\n_start:\n";
-    output << "global _main\n_main:\n";
-    for (int i = 0; i < tokens.size(); ++i)
-    {
-        const Token &token = tokens.at(i);
-        if (token.type == TokenType::EXIT)
-        {
-            if (i+1 < tokens.size() && tokens[i+1].type == TokenType::INT_LIT)
-            {
-                if (i+2 < tokens.size() && tokens[i+2].type == TokenType::SEMI)
-                {
-                    // output << "    mov rax, 60\n";
-                    output << "    mov rax, 0x2000001\n";
-                    output << "    mov rdi, " << tokens[i + 1].value.value() << "\n";
-                    output << "    syscall";
-                }
-            }
-        }
-    }
-
-    return output.str();
-}
-
+// std::string tokens_to_asm(std::vector<Token> &tokens)
+// {
+//     std::stringstream output;
+//     output << "global _start\n_start:\n";
+//     for (int i = 0; i < tokens.size(); ++i)
+//     {
+//         const Token &token = tokens.at(i);
+//         if (token.type == TokenType::EXIT)
+//         {
+//             if (i + 1 < tokens.size() && tokens[i + 1].type == TokenType::INT_LIT)
+//             {
+//                 if (i + 2 < tokens.size() && tokens[i + 2].type == TokenType::SEMI)
+//                 {
+//                     output << "    mov rax, 60\n";
+//                     output << "    mov rdi, " << tokens[i + 1].value.value() << "\n";
+//                     output << "    syscall";
+//                 }
+//             }
+//         }
+//     }
+//
+//     return output.str();
+// }
 
 int main(int argc, char *argv[])
 {
@@ -60,7 +53,8 @@ int main(int argc, char *argv[])
     {
         std::stringstream contents_stream;
         std::fstream input(argv[1], std::ios::in);
-        if (!input.is_open()) {
+        if (!input.is_open())
+        {
             std::cerr << "Error: Could not open file " << argv[1] << std::endl;
             return EXIT_FAILURE;
         }
@@ -68,40 +62,54 @@ int main(int argc, char *argv[])
         contents = contents_stream.str();
     }
 
-    std::vector<Token> tokens = tokenize(contents);
+    Tokenizer tokenizer(std::move(contents));
+
+    std::vector<Token> tokens = tokenizer.tokenize();
+    Parser parser(std::move(tokens));
+    std::optional<NodeExit> tree = parser.parse();
+
+    if (!tree.has_value())
+    {
+        std::cerr << "Error: No exit statement found" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    Generator generator(tree.value());
+
 
     std::string asm_filename = base_filename + ".asm";
     {
         std::fstream file(asm_filename, std::ios::out);
-        if (!file.is_open()) {
+        if (!file.is_open())
+        {
             std::cerr << "Error: Could not create " << asm_filename << std::endl;
             return EXIT_FAILURE;
         }
-        file << tokens_to_asm(tokens);
+        file << generator.generate();
     }
 
     std::string obj_filename = base_filename + ".o";
-    const std::string& exe_filename = base_filename;
+    const std::string &exe_filename = base_filename;
 
-    std::string nasmcmd = "nasm -f macho64 " + asm_filename + " -o " + obj_filename;
-    std::string ldcmd = "ld -platform_version macos 10.7.0 10.15.0 -syslibroot $(xcrun --show-sdk-path) " +
-                        obj_filename + " -o " + exe_filename + " -lSystem";
+    std::string nasmcmd = "nasm -felf64 " + asm_filename + " -o " + obj_filename;
+    std::string ldcmd = "ld " + obj_filename + " -o " + exe_filename;
     std::string rmcmd = "rm -f " + obj_filename + " " + asm_filename;
-
     std::cout << "Executing: " << nasmcmd << std::endl;
     int nasm_result = system(nasmcmd.c_str());
-    if (nasm_result != 0) {
+    if (nasm_result != 0)
+    {
         std::cerr << "nasm failed with error " << nasm_result << std::endl;
         return EXIT_FAILURE;
     }
 
     std::cout << "Executing: " << ldcmd << std::endl;
     int ld_result = system(ldcmd.c_str());
-    if (ld_result != 0) {
+    if (ld_result != 0)
+    {
         std::cerr << "ld failed with error " << ld_result << std::endl;
         return EXIT_FAILURE;
     }
 
-    system(rmcmd.c_str());
+    // system(rmcmd.c_str());
     return EXIT_SUCCESS;
 }
---
+++ src/parser.hpp
@@ -0,0 +1,91 @@
+//
+// Created by balma on 6/11/25.
+//
+#pragma once
+
+#include "tokenization.hpp"
+
+#include <vector>
+
+struct NodeExpr
+{
+    Token int_lit;
+};
+
+struct NodeExit
+{
+    NodeExpr expr;
+};
+
+class Parser
+{
+  public:
+    inline explicit Parser(std::vector<Token> tokens) : m_tokens(std::move(tokens))
+    {
+    }
+
+    std::optional<NodeExpr> parseExpression()
+    {
+        if (peak().has_value() && peak().value().type == TokenType::INT_LIT)
+        {
+            return NodeExpr{.int_lit = consume()};
+        }
+        else
+        {
+            return {};
+        }
+    };
+
+    std::optional<NodeExit> parse()
+    {
+        std::optional<NodeExit> exit_node;
+        while (peak().has_value())
+        {
+            if (peak().value().type == TokenType::EXIT)
+            {
+                consume();
+                if (auto node_expr = parseExpression())
+                {
+                    exit_node = NodeExit{.expr = node_expr.value()};
+                }
+                else
+                {
+                    std::cerr << "Error: Invalid Expression." << std::endl;
+                    exit(EXIT_FAILURE);
+                }
+                if (peak().has_value() && peak().value().type == TokenType::SEMI)
+                {
+                    consume();
+                }
+                else
+                {
+                    std::cerr << "Error: Invalid Expression." << std::endl;
+                    exit(EXIT_FAILURE);
+                }
+            }
+        }
+        m_idx = 0;
+        return exit_node;
+    };
+
+  private:
+    [[nodiscard]] inline std::optional<Token> peak(int ahead = 1) const
+    {
+        if (m_idx + ahead > m_tokens.size())
+        {
+            return {};
+        }
+        else
+        {
+            return m_tokens.at(m_idx);
+        }
+    }
+
+    inline Token consume()
+    {
+        return m_tokens.at(m_idx++);
+    }
+
+    const std::vector<Token> m_tokens;
+    int m_idx = 0;
+};
